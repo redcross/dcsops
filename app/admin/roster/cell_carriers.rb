b1b8277a106536a3fa37f2414dcc3f2ecd61fcd9
John Laxson <jlaxson@mac.com>
2013-06-01 22:21:44 -0700

    Fix editing cell carriers

--- app/admin/roster/cell_carriers.rb
+++ app/admin/roster/cell_carriers.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register Roster::CellCarrier, namespace: 'roster_admin', as: 'Cell Carrier' do
   menu parent: 'Roster'
+
+  controller do
+    def resource_params
+      [params.require(:cell_carrier).permit(:name, :sms_gateway)]
+    end
+  end
 end
