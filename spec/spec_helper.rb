a037b193def2ab1b7851d25cc1db6d06bf510ff4
John Laxson <jlaxson@mac.com>
2013-06-28 11:21:44 -0600

    Try to fix coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+
+require 'coveralls'
+Coveralls.wear! 'rails'
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
@@ -20,9 +24,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
-
-require 'coveralls'
-Coveralls.wear!
 
 RSpec.configure do |config|
   # ## Mock Framework
