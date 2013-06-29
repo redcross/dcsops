6a42deaafc1d1fd87e965e14fb8981c7cda52906
John Laxson <jlaxson@mac.com>
2013-06-29 14:20:32 -0600

    Form proxy wasn't properly returning nested html

--- lib/form_proxy.rb
+++ lib/form_proxy.rb
@@ -15,10 +15,11 @@ class FormProxy
 
   def semantic_fields_for(*args, &block)
     name = args.first
-    target.send( :semantic_fields_for, *args) do |new_builder|
+    target.semantic_fields_for *args do |new_builder|
       deep_proxy = FormProxy.new(new_builder)
-      block.call(deep_proxy)
+      ret = block.call(deep_proxy)
       deep_proxy.keys.each {|deep_key| @keys << :"#{name}.#{deep_key}"}
+      ret
     end
   end
   #alias_method :fields_for, :semantic_fields_for
