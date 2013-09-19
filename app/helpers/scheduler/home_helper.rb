811047f61de649b9e0a9214dc758eaad4815f603
John Laxson <jlaxson@mac.com>
2013-09-18 18:44:30 -0700

    Refactor the on call listings and properly filter out disabled shifts (from #4)

--- app/helpers/scheduler/home_helper.rb
+++ app/helpers/scheduler/home_helper.rb
@@ -22,4 +22,17 @@ module Scheduler::HomeHelper
     end#
     Scheduler::ShiftAssignment.includes{person}.includes{[person.home_phone_carrier, person.cell_phone_carrier, person.work_phone_carrier, person.alternate_phone_carrier, person.sms_phone_carrier]}.where(query)
   end
+
+  def current_shifts_tree(counties, groups=nil)
+    counties = counties.to_a
+    groups ||= Scheduler::ShiftGroup.current_groups_for_chapter(current_person.chapter)
+    groups_by_id = groups.group_by(&:id)
+    shifts_by_county = Scheduler::Shift.where(shift_group_id: groups, county_id: counties).order(:ordinal)
+          .select{|shift| shift.active_on_day?(groups_by_id[shift.shift_group_id].first.start_date)}.group_by(&:county_id)
+    assignments_by_shift = assignments_by_start_date(groups, shifts_by_county.values.flatten.compact).group_by(&:shift_id)
+
+    # Return a hash structure of county => { shift => [assignments]}
+    # There has got to be a better way of doing this...
+    Hash[counties.map{|c| shifts = shifts_by_county[c.id] || []; [c, Hash[shifts.map{|sh| [sh, assignments_by_shift[sh.id] || []]}]]}]
+  end
 end
