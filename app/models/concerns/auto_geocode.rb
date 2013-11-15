8d71c18db0932ddf451fa842897e39253c785e2d
John Laxson <jlaxson@mac.com>
2013-11-15 11:49:30 -0800

    Provision to manually force geocode in AutoGeocode

--- app/models/concerns/auto_geocode.rb
+++ app/models/concerns/auto_geocode.rb
@@ -5,12 +5,12 @@ module AutoGeocode
     before_save :geocode_address
   end
 
-  def geocode_address
+  def geocode_address(force=false)
     return if Rails.env.test?
     return if address1 == 'Address' and city == 'City'
     
-    if lat.nil? or lng.nil? or (changed & %w(address1 address2 city state zip)).present?
-      res = Geokit::Geocoders::GoogleGeocoder3.geocode( [address1, address2, city, state, zip].join(" "))
+    if force or lat.nil? or lng.nil? or (changed & %w(address1 address2 city state zip)).present?
+      res = Geokit::Geocoders::GoogleGeocoder3.geocode( [address1, address2, city, state, zip].compact.join(" "))
       if res
         (self.lat, self.lng) = res.lat, res.lng
       else
