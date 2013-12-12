9795e70016a931faf0dc25005d6200af87e0d017
John Laxson <jlaxson@mac.com>
2013-12-11 16:36:34 -0800

    Only do GC profiling when serving web requests

--- config/initializers/garbage_collection.rb
+++ config/initializers/garbage_collection.rb
@@ -1,1 +1,4 @@
-GC::Profiler.enable if ENV['ENABLE_GC_PROFILER']
+if ENV['ENABLE_GC_PROFILER'] and (defined?(Rails::Server) || defined?(Puma::CLI))
+  puts "Enabling GC Profiling in Web Instance"
+  GC::Profiler.enable
+end
