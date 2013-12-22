b9e6607cc76c3abecf3b63c315f3fe02ac5f429d
John Laxson <jlaxson@mac.com>
2013-12-22 16:04:37 -0700

    Fix including visualization library for gmaps when needed

--- app/helpers/incidents/map_helper.rb
+++ app/helpers/incidents/map_helper.rb
@@ -6,13 +6,13 @@ module Incidents::MapHelper
     {lat: current_chapter.incidents_map_center_lat.try(:to_f), lng: current_chapter.incidents_map_center_lng.try(:to_f), zoom: current_chapter.incidents_map_zoom, geocode_bounds: bounds}
   end
 
-  def google_maps_javascript
+  def google_maps_javascript(libraries=[])
     api_key = ENV['GOOGLE_MAPS_API_KEY']
     if api_key
       api_key = "&client=#{api_key}"
     else
       api_key = ""
     end
-    javascript_include_tag "https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false#{api_key}"
+    javascript_include_tag "https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=#{Array(libraries).join ','}#{api_key}"
   end
 end
