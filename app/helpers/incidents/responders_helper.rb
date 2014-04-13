ce24af8f0e45cf3e777a351d1e88da0e209a4534
John Laxson <jlaxson@mac.com>
2014-04-12 17:48:12 -0700

    Clean up RespondersHelper

--- app/helpers/incidents/responders_helper.rb
+++ app/helpers/incidents/responders_helper.rb
@@ -16,24 +16,31 @@ module Incidents::RespondersHelper
   def person_row(obj, editable = nil)
     person = obj.person
     editable = can?( :create, parent.responder_assignments.build( person: person)) if editable == nil
+
     content_tag :tr, class: 'person', data: {person: person_json(person, obj, editable), person_id: person.id} do
-      content_tag(:td, role_name(obj)) <<
-      content_tag(:td, qualifications(person)) <<
-      content_tag(:td, link_to(person.full_name, person)) <<
-      content_tag(:td) do
-        "#{person.city.try(:titleize)}, #{person.state}" if person.city.present? && person.state.present?
-      end <<
-      tag(:td, class: 'distance') <<
-      tag(:td, class: 'travel-time') <<
-      #content_tag(:td) do
-      #  link_to 'Send SMS', '', class: 'btn btn-mini' if person.sms_addresses.present? && editable
-      #end <<
-      content_tag(:td) do
-        if editable
-          link_to( 'Assign', assignment_url(person, obj), class: 'btn btn-mini', data: {assign: person.id})
-        end
-      end
+      safe_join([
+        content_tag(:td, role_name(obj)),
+        content_tag(:td, qualifications(person)),
+        content_tag(:td, link_to(person.full_name, person)),
+        content_tag(:td, location(person)),
+        tag(:td, class: 'distance'),
+        tag(:td, class: 'travel-time'),
+        #content_tag(:td) do
+        #  link_to 'Send SMS', '', class: 'btn btn-mini' if person.sms_addresses.present? && editable
+        #end <<
+        content_tag(:td, links(person, obj, editable))
+      ])
     end
+  end
+
+  def links(person, obj, editable)
+    if editable
+      link_to('Assign', assignment_url(person, obj), class: 'btn btn-mini', data: {assign: person.id})
+    end
+  end
+
+  def location(person)
+    "#{person.city.try(:titleize)}, #{person.state}" if person.city.present? && person.state.present?
   end
 
   def assignment_url(person, obj)
