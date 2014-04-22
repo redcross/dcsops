8c5bfa1d67212356b4c10916c5f79988ecf71542
John Laxson <jlaxson@mac.com>
2014-04-22 07:04:26 -0700

    Add partner and reserve status as active when importing

--- lib/roster/member_positions_importer.rb
+++ lib/roster/member_positions_importer.rb
@@ -162,7 +162,7 @@ class Roster::MemberPositionsImporter < ImportParser
   end
 
   def is_active_status(status_name)
-    ['General Volunteer', 'Employee'].include? status_name
+    ['General Volunteer', 'General Partner Member', 'Employee', 'NHQ Reserve Employee'].include? status_name
   end
 
   def preload_identities(identities)
