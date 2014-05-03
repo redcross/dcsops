1150e12bfcc2293b467ea80f76806a7573fc8637
John Laxson <jlaxson@mac.com>
2014-05-03 15:22:17 -0700

    Fix assignable select input loading issues

--- app/inputs/assignable_select_admin_input.rb
+++ app/inputs/assignable_select_admin_input.rb
@@ -1,1 +1,4 @@
 require_relative 'assignable_select_input'
+class AssignableSelectAdminInput < Formtastic::Inputs::SelectInput
+  include AssignableSelect
+end
--- app/inputs/assignable_select_input.rb
+++ app/inputs/assignable_select_input.rb
@@ -1,33 +1,3 @@
-module AssignableSelect
-  extend ActiveSupport::Concern
-
-  include ActionView::Helpers::FormOptionsHelper
-  def collection
-    if object.respond_to? humanized_method
-      humanized_collection
-    else
-      assignable_collection
-    end
-  end
-
-  def humanized_method
-    options[:humanized] || "humanized_#{method.to_s.pluralize}"
-  end
-
-  def humanized_collection
-    options_from_collection_for_select(object.send(humanized_method), :value, :humanized, object.send(method))
-  end
-
-  def assignable_collection
-    options_from_collection_for_select(object.send("assignable_#{method.to_s.pluralize}"), :id, :humanized, object.send("#{method}_id"))
-  end
-
-end
-
 class AssignableSelectInput <  FormtasticBootstrap::Inputs::SelectInput
-  include AssignableSelect
-end
-
-class AssignableSelectAdminInput < Formtastic::Inputs::SelectInput
   include AssignableSelect
 end
