83ea63961e890283994b2eac4b9e900f01d91dd5
John Laxson <jlaxson@mac.com>
2014-06-05 20:06:34 -0700

    Have disasters importer count rows

--- lib/incidents/disasters_importer.rb
+++ lib/incidents/disasters_importer.rb
@@ -2,18 +2,19 @@ module Incidents
   class DisastersImporter
 
     def self.get_disasters chapter
-      ImportLog.capture(self.to_s, "Get-#{chapter.id}") do |logger|
+      ImportLog.capture(self.to_s, "Get-#{chapter.id}") do |logger, counter|
         logger.level = 0
         query = Vc::DisasterManagement.new chapter.vc_username, chapter.vc_password, logger
         
         disasters = query.get_active_disasters
-        self.new.import_national disasters
+        self.new.import_national disasters, counter
       end
     end
 
-    def import_national disasters
+    def import_national disasters, counter=nil
       disasters.each do |disaster|
         import_disaster disaster
+        counter.row! if counter
       end
     end
 
