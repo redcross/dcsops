71bfe29402010b95a3677eaab25e89dee800d4aa
John Laxson <jlaxson@mac.com>
2014-06-11 15:22:30 -0400

    Handle cas events returned by api without event number

--- lib/cas/events.rb
+++ lib/cas/events.rb
@@ -7,7 +7,7 @@ module Cas
 
     def find_by_event_number event_number
       resp = client.post '/zf/casevent/search', body: {search_event_number: event_number}, format: :json
-      resp.parsed_response.detect{|json| json['local_event_number'].downcase == event_number.downcase}
+      resp.parsed_response.detect{|json| json['local_event_number'].try(:downcase) == event_number.downcase}
     end
   end
 end
