57efd8ff0cf9bb8ea146127b0b7d2823d986d895
John Laxson <jlaxson@mac.com>
2014-12-08 20:38:13 -0800

    Fix for empty strings not getting coerced to nil

---
+++ config/initializers/clean_empty_strings.rb
@@ -0,0 +1,17 @@
+module CleanEmptyStrings
+  extend ActiveSupport::Concern
+
+  def type_cast_for_write_with_nil_strings(value)
+    if text?
+      value = value.presence
+    end
+    type_cast_for_write_without_nil_strings value
+  end
+
+  included do
+    alias_method_chain :type_cast_for_write, :nil_strings
+  end
+end
+
+ActiveRecord::ConnectionAdapters::Column.send(:include, CleanEmptyStrings)
+ActiveRecord::ConnectionAdapters::PostgreSQLColumn.send(:include, CleanEmptyStrings)
