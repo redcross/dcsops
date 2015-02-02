917355785be08e6ef9f90626e31c56de40d0cf9d
John Laxson <jlaxson@mac.com>
2015-02-01 16:10:46 -0800

    Permit territory_id in notification roles

--- app/admin/incidents/notifications/roles.rb
+++ app/admin/incidents/notifications/roles.rb
@@ -103,7 +103,7 @@ ActiveAdmin.register Incidents::Notifications::Role, as: 'Notification Role' do
     end
 
     def resource_params
-      [params.fetch(resource_request_name, {}).permit(:name, :chapter_id, position_ids: [], shift_ids: [], triggers_attributes: [:id, :event_id, :template, :use_sms, :_destroy], role_scopes_attributes: [:id, :_destroy, :level, :value])]
+      [params.fetch(resource_request_name, {}).permit(:name, :chapter_id, position_ids: [], shift_ids: [], triggers_attributes: [:id, :event_id, :template, :use_sms, :_destroy], role_scopes_attributes: [:id, :_destroy, :level, :value, :territory_id])]
     end
 
     after_build :set_chapter
