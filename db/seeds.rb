364f8c387705acabb88faac4fd421c2737bc4e94
Mike Chu <m.chu90@gmail.com>
2016-01-12 14:44:32 -0600

    Updated sample data
    Deleted grants_role & role_scope methods because the columns were
    removed in db/migrate/20130613191803_create_roster_roles.rb so they're
    now undefined. Updated sample username and added 3 incidents to seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,14 +18,12 @@ so = arcba.counties.create name: 'Solano', vc_regex_raw: 'Solano', abbrev: 'SO'
 mr = arcba.counties.create name: 'Marin', vc_regex_raw: 'Marin', abbrev: 'MR'
 cc = arcba.counties.create name: 'Contra Costa', vc_regex_raw: 'Contra Costa', abbrev: 'CC'
 
-# Commented lines 23-28 because grants_role is an undefined method
-#
-# arcba.positions.create name: 'Chapter Configuration', hidden: true, grants_role: 'chapter_config'
-# arcba.positions.create name: 'Chapter DAT Admin', hidden: true, grants_role: 'chapter_dat_admin'
-# [sf, al, sm, so, mr, cc].each do |county|
-#   arcba.positions.create name: "DAT Administrator - #{county.name}", vc_regex_raw: "#{county.name}.*DAT Administrator$", grants_role: 'county_dat_admin', role_scope: county.id
-#   arcba.positions.create name: "Disaster Manager - #{county.name}", vc_regex_raw: "#{county.name}.*Disaster Manager$", grants_role: 'county_dat_admin', role_scope: county.id
-# end
+arcba.positions.create name: 'Chapter Configuration', hidden: true
+arcba.positions.create name: 'Chapter DAT Admin', hidden: true
+[sf, al, sm, so, mr, cc].each do |county|
+  arcba.positions.create name: "DAT Administrator - #{county.name}", vc_regex_raw: "#{county.name}.*DAT Administrator$"
+  arcba.positions.create name: "Disaster Manager - #{county.name}", vc_regex_raw: "#{county.name}.*Disaster Manager$"
+end
 
 tl = arcba.positions.create name: 'DAT Team Lead', vc_regex_raw: 'Team Lead$'
 tech = arcba.positions.create name: 'DAT Technician', vc_regex_raw: 'Technician$'
@@ -39,10 +37,10 @@ arcba.positions.create name: 'Chapter Vehicle', vc_regex_raw: '^Chapter Vehicle'
 arcba.positions.create name: 'CAC Activator', vc_regex_raw: '^CAC Activator'
 arcba.positions.create name: 'DSHR', vc_regex_raw: 'DSHR'
 
-# day = Scheduler::ShiftGroup.create chapter: arcba, name: 'Day', start_offset: 25200, end_offset: 68400, period: 'daily'
-# night = Scheduler::ShiftGroup.create chapter: arcba, name: 'Night', start_offset: 68400, end_offset: 111600, period: 'daily'
-# week = Scheduler::ShiftGroup.create chapter: arcba, name: 'Weekly', start_offset: 0, end_offset: 7.days, period: 'weekly'
-# month = Scheduler::ShiftGroup.create chapter: arcba, name: 'Monthly', start_offset: 0, end_offset: 31, period: 'monthly'
+day = Scheduler::ShiftGroup.create chapter: arcba, name: 'Day', start_offset: 25200, end_offset: 68400, period: 'daily'
+night = Scheduler::ShiftGroup.create chapter: arcba, name: 'Night', start_offset: 68400, end_offset: 111600, period: 'daily'
+week = Scheduler::ShiftGroup.create chapter: arcba, name: 'Weekly', start_offset: 0, end_offset: 7.days, period: 'weekly'
+month = Scheduler::ShiftGroup.create chapter: arcba, name: 'Monthly', start_offset: 0, end_offset: 31, period: 'monthly'
 
 # Commented lines 49-63 because shift_group is an undefined method
 # 
@@ -67,26 +65,35 @@ arcba.positions.create name: 'DSHR', vc_regex_raw: 'DSHR'
 #vc = Roster::VCImporter.new; 
 #vc.import_data(Roster::Chapter.first, "/Users/jlaxson/Downloads/LMSync1.xls")
 
-me = Roster::Person.find_by_username 'mike'
-me.chapter_id = 1
-me.primary_county_id = 1
-me.username = 'mike'
-me.password = 'test'
-me.first_name = "Michael"
-me.last_name = "Chu"
-me.email = "mike@gmail.com"
-me.home_phone = "909-342-9568"
-me.cell_phone = "312-123-4574"
-me.alternate_phone = "717-224-1150"
-me.sms_phone = "131-234-5444"
-# me.phone_1_preference = "948-234-8592"
-# me.phone_2_preference = "203-023-9586"
-# me.phone_3_preference = "767-219-4857"
-# me.phone_4_preference = "164-283-2324"
-me.address1 = "24 N. State Street"
-me.address2 = "Apt. 001"
-me.city = "Chicago"
-me.state = "IL"
-me.zip = "60611"
-me.save!
+# User Sample Data
+# joe = Roster::Person.find_by_username('joe')
+joe = Roster::Person.create
+joe.chapter_id = 1
+joe.primary_county_id = 1
+joe.username = 'joe'
+joe.password = 'test'
+joe.first_name = "Joe"
+joe.last_name = "Schmo"
+joe.email = "joe@gmail.com"
+joe.home_phone = "909-342-9568"
+joe.cell_phone = "312-123-4574"
+joe.alternate_phone = "717-224-1150"
+joe.sms_phone = "131-234-5444"
+# joe.phone_1_preference = "948-234-8592"
+# joe.phone_2_preference = "203-023-9586"
+# joe.phone_3_preference = "767-219-4857"
+# joe.phone_4_preference = "164-283-2324"
+joe.address1 = "24 N. State Street"
+joe.address2 = "Apt. 001"
+joe.city = "Chicago"
+joe.state = "IL"
+joe.zip = "60611"
+joe.save!
+
+# Incident Sample Data (chapter_id: 1, url_slug: 1, abbrev: "INS1", short_name: "Incident 1", name: "Test Incident 1")
+incident1 = Incidents::Scope.create(chapter_id: 1, url_slug: 1, abbrev: "INS1", short_name: "Incident 1", name: "Test Incident 1")
+incident2 = Incidents::Scope.create(chapter_id: 2, url_slug: 2, abbrev: "INS2", short_name: "Incident 2", name: "Test Incident 2")
+incident3 = Incidents::Scope.create(chapter_id: 3, url_slug: 3, abbrev: "INS3", short_name: "Incident 3", name: "Test Incident 3")
+
+
 
