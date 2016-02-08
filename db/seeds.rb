5b689fbb76db9667d8e353a6152d2efbe65bee03
cecilia-donnelly <cdonnelly@opentechstrategies.com>
2016-02-08 13:39:50 -0600

    Link correctly to dispatch console: #67, #73
    Add an incident scope to the sample data so that the dispatch console
    will work correctly.  Also, fix the console link from the homepage so
    that it can be tested.
    
    Links from the homepage that are still broken and should work:
    scheduler, incidents.  I'll look at those next.
    
    I do not expect links to external services or files to work locally at
    this stage, if ever.

--- db/seeds.rb
+++ db/seeds.rb
@@ -58,9 +58,9 @@ Scheduler::Shift.create county: sf, name: 'Health Services', abbrev: 'DHS', posi
 
 # Add all links to front page:
 
-HomepageLink.create chapter_id: Roster::Chapter.first,  name: 'Volunteer Connection', url: 'http://volunteerconnection.redcross.org', group: 'Other Tools', group_ordinal: 1, ordinal: 1, icon: 'fa fa-2x fa-home'
+HomepageLink.create chapter_id: Roster::Chapter.first,  name: 'Volunteer Connection', url: 'placeholder', group: 'Other Tools', group_ordinal: 1, ordinal: 1, icon: 'fa fa-2x fa-home'
 
-HomepageLink.create chapter_id: Roster::Chapter.first,  name: 'NorCen Dispatch Console', url: 'placeholder', group: 'Dispatch', group_ordinal: 2, ordinal: 1, icon: 'fa fa-2x fa-check-square-o'
+HomepageLink.create chapter_id: Roster::Chapter.first,  name: 'Example Chapter Dispatch Console', url: '/incidents/example_dispatch', group: 'Dispatch', group_ordinal: 2, ordinal: 1, icon: 'fa fa-2x fa-check-square-o'
 HomepageLink.create chapter_id: Roster::Chapter.first,  name: 'Dispatcher Updates', url: 'placeholder', group: 'Dispatch', group_ordinal: 2, ordinal: 2, icon: 'fa fa-2x fa-exclamation-circle'
 HomepageLink.create chapter_id: Roster::Chapter.first,  name: 'Fire Department Contacts', url: 'placeholder', group: 'Dispatch', group_ordinal: 2, ordinal: 3, icon: 'fa fa-2x fa-phone'
 HomepageLink.create chapter_id: Roster::Chapter.first,  name: 'Cas 2.0', url: 'placeholder', group: 'Dispatch', group_ordinal: 2, ordinal: 4, icon: 'fa fa-2x fa-credit-card'
@@ -72,6 +72,9 @@ HomepageLink.create chapter_id: Roster::Chapter.first,  name: 'Response Handbook
 HomepageLink.create chapter_id: Roster::Chapter.first,  name: 'SitCell Common Operating Picture', url: 'placeholder', group: 'SitCell', group_ordinal: 4, ordinal: 1, icon: 'fa fa-2x fa-compass'
 
 HomepageLink.create chapter_id: Roster::Chapter.first,  name: 'DCSOps Training Videos', url: 'placeholder', group: 'Help', group_ordinal: 5, ordinal: 1, icon: 'fa fa-2x fa-youtube'
+
+# Add scope for Dispatch Console 
+Incidents::Scope.create chapter_id: 1, url_slug: 'example_dispatch'
 
 # Create an example user.  Change the credentials here as desired.
 Roster::Person.create(chapter: Roster::Chapter.first, email: "example@example.com", username: "example@example.com", password: "password", last_name: "Example")
