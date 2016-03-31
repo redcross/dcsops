96bfa76231cfe0954ed89622d88b740f0442d88b
cecilia-donnelly <cdonnelly@opentechstrategies.com>
2016-03-31 13:13:06 -0500

    Remove unnecessary migration
    I added this migration in e9927b5 because I thought we needed a separate
    column for day / night times.  It turns out that information is just
    stored in the "config" column, so this additional column is unnecessary.

--- db/migrate/20160202_add_day_night_to_chapter.rb
+++
@@ -1,6 +0,0 @@
-class AddDayNightToChapter < ActiveRecord::Migration
-  def change
-    add_column :roster_chapters, :scheduler_flex_day_start, :integer
-    add_column :roster_chapters, :scheduler_flex_night_start, :integer
-  end
-end
