a90c56e080907e3b52527961882bb915a4412854
Sarah Cnota <scnota@gmail.com>
2016-04-02 17:04:21 -0500

    find calls_with_positive_response - wip (waiting for another commit for the time query)

--- app/models/scheduler/shift_recommendation_score.rb
+++ app/models/scheduler/shift_recommendation_score.rb
@@ -4,4 +4,12 @@ class Scheduler::ShiftRecommendationScore
     rand(5).to_i
   end
 
+  def self.calls_with_positive_response(shift, shift_group, date)
+    Incidents::ResponderAssignment.
+      for_chapter(shift.chapter).
+      was_available.
+      #need to match day of week, time slot, and prev 12 months of shift group and date here
+      count
+  end
+
 end
