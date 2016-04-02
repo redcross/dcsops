a756b90589ae874501a79502d07e16e211cbfa58
Danie Banks <dabanks@thoughtworks.com>
2016-04-02 15:00:39 -0500

    Danie - removed extra data from shift factory

--- spec/factories/scheduler/shifts.rb
+++ spec/factories/scheduler/shifts.rb
@@ -10,14 +10,9 @@ FactoryGirl.define do
     min_desired_signups 1
     ordinal 1
     abbrev 'SH'
-    recommendation_score 1
   end
 
   factory :shift_with_positions, parent: :shift do
     positions { |s| [s.association(:position, chapter: s.county.chapter)] }
-  end
-
-  factory :shift_with_high_recommendation, parent: :shift do
-    recommendation_score 5
   end
 end
