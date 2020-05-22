3baf29f938e35b860c9e23a2ce030e98c84855dc
Frank Duncan <frank@kank.net>
2020-05-22 04:56:00 -0500

    Add SSO login feature tests.
    This requires using the OmniAuth mock capabilities to fake the response
    from Volunteer Connection.

--- spec/features/roster/login_spec.rb
+++ spec/features/roster/login_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "View Timeline", type: :feature, logged_in: false do
 
-  it "Logs in with valid credentials", vcr: {cassette_name: "Vc_Login/makes_a_query_with_uneditable_name"} do
+  it "Logs in legacy mode with valid credentials" do
     person = FactoryGirl.create :person, vc_id: 123123, first_name: 'Bob', last_name: 'Boberson' # Name needs to match the recorded cassette
 
     visit "/?legacy=true"
@@ -17,7 +17,7 @@ describe "View Timeline", type: :feature, logged_in: false do
     expect(person.encrypted_password).not_to be_nil
   end
 
-  it "Logs in with invalid credentials", vcr: {cassette_name: "Vc_Login/incorrect_credentials/raises_an_error"} do
+  it "Logs in legacy mode with invalid credentials", vcr: {cassette_name: "Vc_Login/incorrect_credentials/raises_an_error"} do
     person = FactoryGirl.create :person, vc_id: 123123, first_name: 'Bob', last_name: 'Boberson' # Name needs to match the recorded cassette
 
     visit "/?legacy=true"
@@ -29,5 +29,29 @@ describe "View Timeline", type: :feature, logged_in: false do
     person.reload
     expect(person.username).to be_nil
     expect(person.encrypted_password).to be_nil
+  end
+
+  it "Logs in via single signon" do
+    FactoryGirl.create(:person, rco_id: 12345, first_name: "Test", last_name: "User")
+
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:default] = OmniAuth::AuthHash.new({
+      :uid => '12345'
+    })
+    visit "/"
+    click_on "Log in with Red Cross Single Sign On"
+
+    page.should have_text("Test User")
+  end
+
+  it "Logs in via single signon with no database entry" do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:default] = OmniAuth::AuthHash.new({
+      :uid => '12345'
+    })
+    visit "/"
+    click_on "Log in with Red Cross Single Sign On"
+
+    page.should have_text("Please Sign In")
   end
 end
