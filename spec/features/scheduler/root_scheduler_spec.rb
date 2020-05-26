610f99e45054e52a56248aa94cf04f2139700994
Frank Duncan <frank@kank.net>
2020-05-26 05:28:11 -0500

    Add first scheduler feature tests
    These test the scheduler is working, and basic viewing of the flex
    schedule, as well as the general schedule.

---
+++ spec/features/scheduler/root_scheduler_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe "Root Scheduler Page", :type => :feature do
+  it "Visits the scheduler page" do
+    visit "/"
+
+    within "div.app", text: "DAT Scheduling" do
+      click_on "DAT Scheduling"
+    end
+
+    expect(page).to have_current_path("/scheduler")
+  end
+
+  it "Views the default page" do
+    visit "/scheduler/"
+
+    # This isn't a comprehensive check, but should suffice that the
+    # page is actually working.
+    page.should have_text "You have completed 0 shifts since the new"
+    page.should have_text "You have no upcoming shifts scheduled."
+
+    # Flex schedule should be empty by default
+    find(".flex-small").should_not have_text "Yes"
+    find(".flex-small").should have_text("No", count: 14)
+  end
+
+  it "Views the default page with shifts" do
+    shift = FactoryGirl.create :shift, county: @person.counties.first, positions: @person.positions
+    shift_group = shift.shift_groups.first
+    (1..5).map{|i| Scheduler::ShiftAssignment.create! person:@person, date:Date.today+i, shift:shift, shift_group:shift_group}
+
+    visit "/scheduler/"
+
+    page.should_not have_text "You have no upcoming shifts scheduled."
+  end
+
+  it "Views the default page with completed shifts" do
+    shift = FactoryGirl.create :shift, county: @person.counties.first, positions: @person.positions
+    shift_group = shift.shift_groups.first
+    (1..5).map{|i| Scheduler::ShiftAssignment.create! person:@person, date:Date.today-i, shift:shift, shift_group:shift_group}
+
+    visit "/scheduler/"
+
+    page.should have_text "You have completed 5 shifts since the new scheduling system was launched."
+  end
+end
