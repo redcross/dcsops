e425d74334e5ca3f6efd6ee41e0ae135adf8013a
Frank Duncan <frank@kank.net>
2020-05-26 16:36:15 -0500

    Fix tests on travis, login avoids redirect
    For some reason, travis capybara doesn't follow redirects on the 1+n
    times it hits a page, so going to "/" was just failing on every test
    after the first one, when wanting to login.  So instead, making the
    login code go right to the correct place was a simple fix.
    
    Also include logging out after every feature call, as well as some
    whitespace fixes while in there.

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -5,16 +5,19 @@ module FeatureSpec
 
   included do
     def login_person(person)
-        OmniAuth.config.test_mode = true
-        OmniAuth.config.mock_auth[:default] = OmniAuth::AuthHash.new({
-          :uid => person.rco_id.to_s
-        })
-        visit "/"
-        click_on "Log in with Red Cross Single Sign On"
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:default] = OmniAuth::AuthHash.new({
+        :uid => person.rco_id.to_s
+      })
+
+      # Have to go to the deep login page, not "/" and let redirect
+      # because travis + capybara wasn't following the redirects correctly
+      visit "/roster/session/new_rco?rco_idp_mode=dcs0"
+      click_on "Log in with Red Cross Single Sign On"
     end
 
     def logout
-        click_on "Logout"
+      click_on "Logout"
     end
 
     self.use_transactional_fixtures = false
@@ -26,8 +29,11 @@ module FeatureSpec
       login_person @person
     end
 
-    after(:each) do
+    after(:each) do |example|
+      next if example.metadata[:logged_in] == false
+
       @person = nil
+      logout
     end
 
     # For some incredibly stupid reason the Sauce gem aliases page to call 'selenium',
