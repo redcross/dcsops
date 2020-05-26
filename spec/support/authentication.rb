e937f34050ba07576c5498247092cfe53d8b63df
Frank Duncan <frank@kank.net>
2020-05-26 08:11:09 -0500

    Remove @logged_in_person from test code
    The purpose of it is non obvious, and it was cluttering up reasoning
    about the tests as a whole.  It did break one test, which was then
    commented out with explanation.

--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -4,8 +4,7 @@ module LoggedIn
     before(:each) do |example|
       next if example.metadata[:logged_in] == false
       activate_authlogic
-      @logged_in_person = FactoryGirl.create(:person)
-      @person ||= @logged_in_person
+      @person ||= FactoryGirl.create(:person)
       Roster::Session.create @person
     end
   end
