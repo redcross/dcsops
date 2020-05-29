e513256a1721cabac0236b50085c47d590e891a4
Frank Duncan <frank@kank.net>
2020-05-29 06:05:37 -0500

    Add base feature test for viewing admin area

---
+++ spec/features/admin/root_admin_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "Root Admin Page", :type => :feature do
+  it "Visits the admin page" do
+    # Can't visit till enabled
+    visit "/"
+
+    page.should_not have_text("Admin")
+    grant_role! :chapter_config
+
+    visit "/"
+    click_on "Admin"
+
+    expect(page).to have_current_path("/scheduler_admin/people")
+  end
+end
