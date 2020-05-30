285e5d14aa3e040e0f5a0fb2bae80d4f0e359798
Frank Duncan <frank@kank.net>
2020-05-30 05:42:07 -0500

    Add basic homepage links admin test

---
+++ spec/features/admin/system_homepage_links_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe "System Homepage Links Admin Page", :type => :feature do
+  before(:each) do
+    grant_role! :chapter_config
+  end
+
+  it "Creates a new Homepage Link" do
+    visit "/scheduler_admin/homepage_links"
+
+    click_on "Create one"
+    fill_in "Name", with: "A New Homepage Link"
+    within first "#homepage_link_submit_action" do
+      click_on ("Create Homepage link")
+    end
+    page.should have_text("A New Homepage Link")
+
+    click_on "Homepage Links"
+    # Make sure list page fully loads
+    expect(page).to have_current_path("/scheduler_admin/homepage_links")
+  end
+end
