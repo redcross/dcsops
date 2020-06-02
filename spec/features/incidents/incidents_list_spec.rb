7ef7a61e0dda60ca0d18d83e4194ba2a5d468171
Frank Duncan <frank@kank.net>
2020-06-02 14:14:19 -0500

    Add basic incident list spec

---
+++ spec/features/incidents/incidents_list_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "Incidents List", :type => :feature do
+  it "Should be viewable" do
+    FactoryGirl.create :incidents_scope, chapter: @person.chapter
+    puts "/incidents/#{@person.chapter.url_slug}"
+    visit "/incidents/#{@person.chapter.url_slug}"
+    click_on "More Incidents"
+
+    page.should have_text "Listing 0 Incidents"
+  end
+end
