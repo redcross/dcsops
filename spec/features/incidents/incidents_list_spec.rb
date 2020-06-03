0c18a34ba59b9ab9d1240161c33266d22522c70b
Frank Duncan <frank@kank.net>
2020-06-03 06:36:05 -0500

    Remove extraneous output

--- spec/features/incidents/incidents_list_spec.rb
+++ spec/features/incidents/incidents_list_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe "Incidents List", :type => :feature do
   it "Should be viewable" do
     FactoryGirl.create :incidents_scope, chapter: @person.chapter
-    puts "/incidents/#{@person.chapter.url_slug}"
     visit "/incidents/#{@person.chapter.url_slug}"
     click_on "More Incidents"
 
