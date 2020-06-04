aeb89e44006b531c2d98b6574f24e03fe1caa80f
Frank Duncan <frank@kank.net>
2020-06-04 07:53:33 -0500

    Add basic notification settings feature test

---
+++ spec/features/scheduler/notifications_settings_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "Notifications Settings Page", :type => :feature do
+  it "Views the default page as admin" do
+    grant_role! 'chapter_dat_admin'
+
+    visit "/scheduler/"
+    click_on "Update reminder preferences"
+
+    page.should have_text "Reminder Settings"
+  end
+
+  it "Uses the me id" do
+    visit "/scheduler/notification_settings/me"
+
+    page.should have_text "Reminder Settings"
+  end
+
+  it "Updates an option" do
+    visit "/scheduler/notification_settings/me"
+
+    click_on "scheduler_notification_setting_send_email_invites"
+    find(".form-control").select("Do")
+    find(".glyphicon-ok").find(:xpath, ".//..").click
+
+    page.should have_text("Do not", count: 1)
+  end
+end
