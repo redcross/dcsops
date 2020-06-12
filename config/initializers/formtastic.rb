2072cfbd756e7f4fce21b724291e006f1a105263
Todd Eichel <todd@toddeichel.com>
2020-06-12 12:25:29 -0700

    Rails 5 upgrade: update formtastic and related
    This removes version constraints and `bundle updates` formtastic,
    formtastic-bootstrap, and cocoon.
    
    It removes some dev mode instrumentation which was not working and
    seems non-critical (`undefined method `add_method_tracer' for
    FormtasticBootstrap::Inputs::BooleanInput:Class`).
    
    Support:
    
    - formtastic: supports up to Rails 6 explicitly
    
    - formtastic-bootstrap: no explicit support or recent commits, and no
      obvious replacement, however the app does boot with this in place.
      However I've not rendered any forms so I have no idea if view
      rendering will crash.
    
    - cocoon: expresses explicit support for Rails 5 and may incidentally
      work on 6

--- config/initializers/formtastic.rb
+++
@@ -1,8 +0,0 @@
-if Rails.env.development?
-  FormtasticBootstrap::Inputs.constants.map{|c| FormtasticBootstrap::Inputs.const_get(c)}.select {|c| Class === c}.each do |klass|
-   klass.class_eval {
-    #add_method_tracer :to_html, "Custom/Formtastic/Bootstrap/#{klass.to_s}/to_html", :metric => false, push_scope: false
-    add_method_tracer :to_html, "Custom/Formtastic/Bootstrap/Input/to_html", :metric => false
-  }
-  end
-end
