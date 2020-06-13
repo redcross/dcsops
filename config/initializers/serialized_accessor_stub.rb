d431dbfdd56f19384d86b542fa54b7c014279953
Todd Eichel <todd@toddeichel.com>
2020-06-12 18:34:20 -0700

    Rails 5 upgrade: warn on usage of SerializedColumns
    Remind ourselves that we disabled all these methods, and it might cause
    unexpected behavior when various config options are all returning nil.

--- config/initializers/serialized_accessor_stub.rb
+++ config/initializers/serialized_accessor_stub.rb
@@ -6,20 +6,30 @@ module Core
 
         serialized_columns[name] = [store_attribute, column]
 
+        serialized_columns_warning = -> { ActiveSupport::Deprecation.warn("SerializedColumns is currently stubbed out, this method is a no-op") }
+
         define_method name do
+          serialized_columns_warning.call
+          nil
         end
 
         define_method :"#{name}_before_type_cast" do
+          serialized_columns_warning.call
+          nil
         end
 
         define_method :"#{name}=" do |val|
+          serialized_columns_warning.call
+          nil
         end
 
         scope :"with_#{name}_present", -> do
+          serialized_columns_warning.call
           none
         end
 
         scope :"with_#{name}_value", ->(val) do
+          serialized_columns_warning.call
           none
         end
       end
