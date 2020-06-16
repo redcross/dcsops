262740ae4e25538e0fcd98957c912fdec5754029
Todd Eichel <todd@toddeichel.com>
2020-06-15 17:41:54 -0700

    Unsqueel: replace all uses of `group`
    This replaces Squeel `.group{}` calls with vanilla AR versions.

--- app/models/scheduler/shift.rb
+++ app/models/scheduler/shift.rb
@@ -124,7 +124,7 @@ class Scheduler::Shift < ApplicationRecord
     arr = Scheduler::ShiftAssignment
       .where(shift_id: shifts.to_a, date: month..(month.at_end_of_month))
       .select('COUNT(id) AS count, shift_id, shift_time_id, date')
-      .group{[shift_id, shift_time_id, date]}
+      .group(:shift_id, :shift_time_id, :date)
       .reduce(arr) do |hash, ass|
       shift = shifts_by_id[ass.shift_id]
       group = groups_by_id[ass.shift_time_id]
