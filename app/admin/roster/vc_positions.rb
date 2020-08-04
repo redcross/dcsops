755274f4041501fbfcacc83a6e0d81ec89283522
Frank Duncan <frank@kank.net>
2020-08-04 12:32:06 -0500

    Add a vc position admin page
    Issue #255: Write scripts from Red Cross-provided spreadsheets and
                import regional configuration data into DCSOps

---
+++ app/admin/roster/vc_positions.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register Roster::VcPosition, as: 'VC Position' do
+  menu parent: 'Roster'
+
+  filter :region
+  filter :name
+  filter :positions
+  filter :shift_territories
+
+  show do
+    default_main_content
+
+    panel "Configuration" do
+      table_for resource.vc_position_configurations do
+        column("Position") { |r| r.position.name }
+        column("Shift Territory") { |r| r.shift_territory.name }
+      end
+    end
+  end
+
+  form do |f|
+    f.inputs
+    f.inputs do
+      f.has_many :vc_position_configurations, allow_destroy: true do |f|
+        f.input :position
+        f.input :shift_territory
+      end
+    end
+    f.actions
+  end
+
+end
--- app/models/roster/vc_position.rb
+++ app/models/roster/vc_position.rb
@@ -1,4 +1,9 @@
 class Roster::VcPosition < ActiveRecord::Base
   belongs_to :region, class_name: 'Roster::Region'
   has_many :vc_position_configurations
+
+  has_many :positions, through: :vc_position_configurations
+  has_many :shift_territories, through: :vc_position_configurations
+
+  accepts_nested_attributes_for :vc_position_configurations, allow_destroy: true
 end
