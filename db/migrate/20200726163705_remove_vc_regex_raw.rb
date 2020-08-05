83d190ec3446db773c88276be87621958b124e74
Frank Duncan <frank@kank.net>
2020-08-05 11:53:20 -0500

    Make vc regex raw removal migration rollbackable

--- db/migrate/20200726163705_remove_vc_regex_raw.rb
+++ db/migrate/20200726163705_remove_vc_regex_raw.rb
@@ -1,6 +1,6 @@
 class RemoveVcRegexRaw < ActiveRecord::Migration
   def change
-    remove_column :roster_shift_territories, :vc_regex_raw
-    remove_column :roster_positions, :vc_regex_raw
+    remove_column :roster_shift_territories, :vc_regex_raw, :string
+    remove_column :roster_positions, :vc_regex_raw, :string
   end
 end
