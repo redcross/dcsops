7c013242885b41956df4a4011c0d419833715fc1
Frank Duncan <frank@kank.net>
2021-02-24 10:38:06 -0600

    Add responders history date search, csv
    Issue #301: Built-in report pages, to replace dependency on external query
                dashboards.

--- app/controllers/incidents/responses_controller.rb
+++ app/controllers/incidents/responses_controller.rb
@@ -1,9 +1,25 @@
 class Incidents::ResponsesController < Incidents::BaseController
+  
+  #respond_to :html, :csv
 
   has_scope :with_person_in_shift_territories, as: :shift_territory_id, default: ->controller{controller.current_user.primary_shift_territory_id}
-  has_scope :response_in_last, default: 180 do |controller, scope, val|
-    date = Date.current - val.to_i
-    scope.left_outer_joins(:incident).where(incidents_incidents: { date: date..DateTime::Infinity.new })
+  has_scope :date_after, :allow_blank => true, :default => FiscalYear.current.start_date.to_s do |controller, scope, val|
+    if not controller.params[:date_before].blank?
+      scope.left_outer_joins(:incident).where(incidents_incidents: { date: val..controller.params[:date_before] })
+    elsif not val.blank?
+      scope.left_outer_joins(:incident).where("incidents_incidents.date > ?", val)
+    else
+      scope
+    end
+  end
+  has_scope :date_before do |controller, scope, val|
+    # This is a hack, I guess, but I don't know how to do it otherwise
+    # If there date_after is present, we have to let that scope handle everything
+    if not controller.params[:date_after].blank?
+      scope
+    else
+      scope.left_outer_joins(:incident).where("incidents_incidents.date < ?", val)
+    end
   end
 
   expose(:responders) {
